5f28d1405aa42242881ada9d0b860683f72f6a12
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2019-02-24 00:37:05 +0530

    Initial Phase is done
    Signed-off-by: Jawadh-Salih <jawadhsalih.4th@gmail.com>

---
+++ about.php
@@ -0,0 +1,41 @@
+<?php require 'includes/header.php'?>
+
+<?php require 'includes/navbar.php'?>
+
+<body>
+    <!-- About -->
+    <section id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">About</h2>
+            <h3 class="section-subheading text-muted">Gothatuwa Youth Development Association is a volunteer Organization who serves for the well being 
+            of the community around Gothatuwa.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <!-- Team -->
+    <!-- <section class="bg-light" id="team">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Executive Committee. </h2>
+             <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> 
+          </div>
+        </div>
+       
+        
+      </div>
+    </section> -->
+
+    <?php // include "includes/footer.php" ?>
+
+</body>
--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php require 'includes/header.php'?>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <?php include "includes/navbar.php" ?> 
+    
+
+    <!-- Header -->
+    <header class="masthead">
+    <img src="img/header-bg2.jpg"/>
+
+      <div class="container">
+        <!-- <div class="intro-text"> -->
+          <!-- <div class="intro-lead-in">Welcome To Our Studio!</div>
+          <div class="intro-heading text-uppercase">It's Nice To Meet You</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a> -->
+        <!-- </div> -->
+      </div>
+    </header>
+    <?php // include "includes/footer.php" ?>
+  </body>
+
+</html>
