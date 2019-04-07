558037c21a0cad930197010e3c3c5db85aa67ba3
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2019-04-07 22:58:48 +0530

    About us updated and further features added
    Signed-off-by: Jawadh-Salih <jawadhsalih.4th@gmail.com>

--- about.php
+++ about.php
@@ -9,8 +9,14 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">About</h2>
-            <h3 class="section-subheading text-muted">Gothatuwa Youth Development Association is a volunteer Organization who serves for the well being 
-            of the community around Gothatuwa.</h3>
+            <h3 class="section-subheading text-muted">Gothatuwa Youth Development Association well known as "GYDA", 
+            established in 2018 with consists of Youth from the locality of Gothatuwa area. GYDA organization is dedicated 
+            to the collective wisdom and efforts towards the prosperity & development of Gothatuwa Area and mother country. 
+            Together GYDA organizes educational, sports and self-development events, 
+            religious and social programs for the benefit of its members and people of Gothatuwa.</h3>
+            <div>
+              <img src="img/team/GYDA_TEAM.jpg"/>
+            </div>
           </div>
         </div>
         <div class="row">
--- includes/navbar-elements.php
+++ includes/navbar-elements.php
@@ -7,8 +7,10 @@
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="gallery.php">Gallery</a>
           </li>
-          <!-- <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="contact.php">Contact</a>
-          </li> -->
+          <li class="nav-item">
+
+            <a class="nav-link js-scroll-trigger" href="docs/Cons_26.01.2019.pdf" download>
+            <i class="fa fa-download" style="font-size:18px"></i> Our Constitution</a>
+          </li>
         </ul>
       </div>
